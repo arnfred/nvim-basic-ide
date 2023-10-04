0a4694295b069465a2286a129d9c9d0553a3d581
Jonas Arnfred <ja@gower.st>
2023-10-04 11:23:42 +0100

    Added conjure

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- example using a list of specs with the default options
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = ","
 
 -- load lazy
 require("lazy").setup("user", {
