b15e89f0666c38aec2f7911d236c606b452fa2ae
Jonas Arnfred <ja@gower.st>
2025-02-05 17:07:50 +0000

    Open and close aider with '\'

--- lua/plugins/aider.lua
+++ lua/plugins/aider.lua
@@ -4,16 +4,14 @@ local M = {
         "AiderTerminalToggle", "AiderHealth",
     },
     keys = {
-        { "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
-        { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
-        { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
-        { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-        { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-        { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
-        { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-        -- Example nvim-tree.lua integration if needed
-        { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
-        { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+        { "\\", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider", mode = { "n", "v" } },
+        { "<localleader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+        { "<localleader>\\", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
+        { "<localleader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
+        { "<localleader>=", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+        { "<localleader>-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+        { "<localleader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
+        { "\\", "<C-\\><C-n><cmd>AiderTerminalToggle<cr>", mode = "t" },
     },
     dependencies = {
         "folke/snacks.nvim",
@@ -29,7 +27,21 @@ function M.config()
     require("nvim_aider").setup({
         win = {
             position = "right"
-        }
+        },
+        config = {
+            keys = {
+                term_normal = {
+                    "<esc>",
+                    function(self)
+                        self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+                        vim.cmd("stopinsert")
+                    end,
+                    mode = "t",
+                    expr = true,
+                    desc = "Double escape to normal mode",
+                },
+            },
+        },
     })
 end
 
